b66de37fb4b9388ac0dd5844bd5ec693131bd17b
Mihael Bun <bunmihail@gmail.com>
2022-09-08 17:56:38 +0300

    Task 9.4 complete

---
+++ 9_Lesson/9.4/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая на вход принимает два числа A и B, и
+// возводит число А в целую степень B с помощью рекурсии.
+
+
+int Degree(int a, int b)
+{
+    if(b <= 1) return a;
+    return  a * Degree(a, b - 1);
+}
+
+Console.WriteLine(Degree(2,5));
